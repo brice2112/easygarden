82647e397e0a9a99807bd326a046292fbcb6f103
brice blanchard <brice.blanchard21@yahoo.fr>
2023-06-07 17:54:26 +0200

    finished garden creation route

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,5 +1,8 @@
+require 'net/http'
+require 'json'
+
 class GardensController < ApplicationController
-  before_action :set_garden, only: [:show, :destroy]
+  before_action :set_garden, only: [:show, :destroy, :garden_created]
   COMP_W = 1
   IMP_L = 1
   ALLEY_W = 0.5
@@ -11,6 +14,12 @@ class GardensController < ApplicationController
   def show
   end
 
+  def garden_created
+    gps_coords = get_gps_coord(@garden.location)
+    mean_temp = get_mean_temp(gps_coords[0], gps_coords[1])
+    @suitable_vegetables = get_suitable_vegetables(mean_temp)
+  end
+
   def new
     @garden = Garden.new
   end
@@ -19,22 +28,25 @@ class GardensController < ApplicationController
 
     @garden = Garden.new(garden_params)
     @garden.user = current_user
+
+    # Calculate number of compartments
     n = number_of_compartments(@garden.width)
     m = number_of_implantations(@garden.length)
     i = 0
     while (i <= n) do
-      compartment = @garden.compartments.new
-      j = 0
-      while (j <= m) do
-        implantation = compartment.implantations.new
-        implantation.vegetable = Vegetable.first
-        j += 1
-      end
+      @garden.compartments.new
+      # j = 0
+      # while (j <= m) do
+      #   implantation = compartment.implantations.new
+      #   implantation.vegetable = Vegetable.first
+      #   j += 1
+      # end
       i += 1
     end
 
     if @garden.save!
-      redirect_to garden_path(@garden.id)
+      redirect_to garden_created_path(@garden)
+      # raise
     else
       render new, status: :unprocessable_entity
     end
@@ -43,7 +55,6 @@ class GardensController < ApplicationController
   def destroy
   end
 
-
   private
 
   def set_garden
@@ -51,7 +62,7 @@ class GardensController < ApplicationController
   end
 
   def garden_params
-    params.require(:garden).permit(:length, :width, :address, :name)
+    params.require(:garden).permit(:length, :width, :location, :name)
   end
 
   def number_of_compartments(garden_width)
@@ -62,6 +73,51 @@ class GardensController < ApplicationController
   def number_of_implantations(garden_length)
     imp_num = garden_length
     imp_num.to_i
+  end
+
+  def get_gps_coord(address)
+      output = []
+      result = Geocoder.search(address).first
+      if result
+        latitude = result.latitude
+        longitude = result.longitude
+        output = [latitude, longitude]
+      end
+      output
+  end
+
+  def get_mean_temp(latitude, longitude)
+    result = nil
+
+    # API endpoint URL
+    url = "https://api.open-meteo.com/v1/forecast?latitude=#{latitude}&longitude=#{longitude}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&past_days=92&forecast_days=16&timezone=Europe%2FLondon"
+
+    # Make an HTTP GET request to the API
+    response = Net::HTTP.get(URI(url))
+
+    # Parse the JSON response
+    data = JSON.parse(response)
+
+    # Extract temperature data and calculate averages
+    if data['daily']
+      max_temp = data['daily']['temperature_2m_max']
+      min_temp = data['daily']['temperature_2m_min']
+      max_mean_temp = max_temp.inject{ |sum, el| sum + el }.to_f / max_temp.size
+      min_mean_temp = min_temp.inject{ |sum, el| sum + el }.to_f / min_temp.size
+      mean_temp = (max_mean_temp + min_mean_temp) / 2
+      result = mean_temp.truncate(2)
+    end
+    result
+  end
+
+  def get_suitable_vegetables(mean_temp)
+    suitable_vegetables = []
+    Vegetable.all.each do |vegetable|
+      if mean_temp > vegetable.min_temp && mean_temp < vegetable.max_temp
+          suitable_vegetables.append(vegetable)
+      end
+    end
+    return suitable_vegetables
   end
 
 end
