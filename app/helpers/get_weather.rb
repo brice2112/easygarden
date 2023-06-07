06e59a9279bb4df5771f6d7077830151d1f5996d
brice blanchard <brice.blanchard21@yahoo.fr>
2023-06-07 14:25:11 +0200

    added get weather feature

--- app/helpers/get_weather.rb
+++ app/helpers/get_weather.rb
@@ -1,10 +1,12 @@
 require 'net/http'
 require 'json'
+# require 'awesome_print'
 
 def get_weather(latitude, longitude)
-  api_key = WEATHER
+  result = nil
+
   # API endpoint URL
-  url = "https://api.openweathermap.org/data/2.5/weather?lat=#{latitude}&lon=#{longitude}&appid=#{api_key}"
+  url = "https://api.open-meteo.com/v1/forecast?latitude=#{latitude}&longitude=#{longitude}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&past_days=92&forecast_days=16&timezone=Europe%2FLondon"
 
   # Make an HTTP GET request to the API
   response = Net::HTTP.get(URI(url))
@@ -12,26 +14,14 @@ def get_weather(latitude, longitude)
   # Parse the JSON response
   data = JSON.parse(response)
 
-  if data['cod'] == 200
-    # Extract relevant weather information from the response
-    temperature = data['main']['temp']
-    weather_description = data['weather'][0]['description']
-    humidity = data['main']['humidity']
-
-    # Return the weather data
-    return {
-      temperature: temperature,
-      description: weather_description,
-      humidity: humidity
-    }
-  else
-    # Handle API error response
-    error_message = data['message']
-    puts "Error: #{error_message}"
-    return nil
+  # Extract temperature data and calculate averages
+  if data['daily']
+      max_temp = data['daily']['temperature_2m_max']
+      min_temp = data['daily']['temperature_2m_min']
+      max_mean_temp = max_temp.inject{ |sum, el| sum + el }.to_f / max_temp.size
+      min_mean_temp = min_temp.inject{ |sum, el| sum + el }.to_f / min_temp.size
+      mean_temp = (max_mean_temp + min_mean_temp) / 2
+      result = mean_temp.truncate(2)
   end
+  result
 end
-
-
-# API request example :
-"https://history.openweathermap.org/data/2.5/aggregated/year?lat=35&lon=139&appid=5e5ae1e28bd8f7d9558fa46d0e022d64"
