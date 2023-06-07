b92d8dc085ed79fd192854bf83c36ba64f90b61d
brice blanchard <brice.blanchard21@yahoo.fr>
2023-06-07 14:57:50 +0200

    added temperature migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_090215) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_125602) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_090215) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "mean_temperature"
     t.index ["user_id"], name: "index_gardens_on_user_id"
   end
 
