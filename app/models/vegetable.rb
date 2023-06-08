96347078d6488c6ad80571253879926333c84554
Helene Coulombeix <helenecoulombeix@yahoo.fr>
2023-06-08 18:05:44 +0200

    clefcloudinary

--- app/models/vegetable.rb
+++ app/models/vegetable.rb
@@ -1,6 +1,9 @@
 class Vegetable < ApplicationRecord
   has_many :implantations
   has_many :vegetables, through: :synergies
+
+  has_one_attached :photo
+  
   def synergies
     Synergy.where("first_vegetable_id = :id OR second_vegetable_id = :id", id: id)
   end
