13f0b46df168afd357116fc26df0bcd0209a410f
brice blanchard <brice.blanchard21@yahoo.fr>
2023-06-12 16:11:14 +0200

    seed fixed with images

--- db/seeds.rb
+++ db/seeds.rb
@@ -76,7 +76,7 @@ file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686312702/c
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Pomme de terre",
   variety: "Solanaceae",
   min_temp: 10,
@@ -87,12 +87,11 @@ Vegetable.create!(
   footprint: 0.35,
   description:"La pomme de terre est un légume-racine polyvalent et largement consommé dans le monde entier. Elle est connue pour sa texture féculente et son goût délicieux. Les pommes de terre sont une excellente source d'énergie grâce à leur teneur élevée en glucides complexes. Elles contiennent également des vitamines et des minéraux essentiels tels que la vitamine C, le potassium et le fer. Les pommes de terre peuvent être préparées de nombreuses façons différentes, comme bouillies, rôties, en purée, en frites ou même en gratin. Elles sont un ingrédient de base dans de nombreux plats populaires tels que les plats de pâtes, les ragoûts, les potages et bien d'autres."
 )
-
 file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686312753/pommedeterre_dyfyn1.jpg')
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Chou",
   variety: "brassicacées",
   min_temp: 7,
@@ -107,7 +106,7 @@ file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686312721/c
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Brocoli",
   variety: "crucifères",
   min_temp: 7,
@@ -119,12 +118,11 @@ Vegetable.create!(
   description:"
   Le brocoli est un légume vert à fleurs de la famille des crucifères. Il est réputé pour sa forme de petit arbre et est largement apprécié pour sa saveur délicieuse et ses nombreux bienfaits pour la santé. Le brocoli est une excellente source de vitamines, de minéraux et d'antioxydants. Il est également riche en fibres alimentaires et pauvre en calories, ce qui en fait un choix idéal pour une alimentation saine. Le brocoli peut être consommé cru, cuit à la vapeur, sauté ou ajouté à une variété de plats tels que les salades, les soupes, les sautés et les plats d'accompagnement."
 )
-
 file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686312695/broccoli_ao3ab2.jpg')
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Chou-fleur",
   variety: "crucifères",
   min_temp: 7,
@@ -135,12 +133,11 @@ Vegetable.create!(
   footprint: 0.4,
   description: "Le chou est un légume à feuilles denses et croquantes qui appartient à la famille des crucifères. Il existe différentes variétés de chou, comme le chou vert, le chou rouge et le chou frisé. Le chou est connu pour sa saveur distinctive, légèrement amère, ainsi que pour sa texture ferme. C'est un légume nutritif, riche en vitamines C, K et en fibres alimentaires. Le chou peut être consommé cru, dans des salades, ou cuit dans divers plats tels que les sautés, les soupes, les ragoûts et les plats d'accompagnement. Il est également utilisé pour la préparation de choucroute et d'autres plats fermentés. Le chou est apprécié pour sa polyvalence culinaire et ses bienfaits pour la santé."
 )
-
 file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686312733/choufleur_edvnxn.jpg')
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Epinard",
   variety: "Amaranthacées",
   min_temp: 7,
@@ -151,12 +148,11 @@ Vegetable.create!(
   footprint: 0.1,
   description:"Les épinards sont de délicieuses feuilles vertes qui appartiennent à la famille des Amaranthacées. Ils sont appréciés pour leur goût légèrement terreux et leur texture tendre. Les épinards sont une excellente source de nutriments, notamment de vitamines A, C et K, de fer, de calcium et de fibres alimentaires. Ils sont connus pour leurs nombreux bienfaits pour la santé, notamment leur capacité à renforcer le système immunitaire, à favoriser la santé des os et à améliorer la santé oculaire. Les épinards peuvent être consommés crus dans les salades ou cuits à la vapeur, sautés, ou ajoutés à une variété de plats tels que les soupes, les plats d'accompagnement, les omelettes et les smoothies. Ils sont un ajout nutritif et savoureux à une alimentation équilibrée."
 )
-
 file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686312752/epinard_wzqehp.jpg')
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Laitue",
   variety: "Asteraceae",
   min_temp: 5,
@@ -167,12 +163,11 @@ Vegetable.create!(
   footprint: 0.15,
   description: "La laitue est un légume à feuilles vertes largement consommé dans le monde entier. Elle se caractérise par sa texture croquante et son goût frais et légèrement amer. La laitue est souvent utilisée comme base pour les salades, offrant une grande variété de types et de variétés, tels que la laitue iceberg, la laitue romaine et la laitue frisée. La laitue est faible en calories et riche en eau, ce qui en fait un choix sain pour les personnes soucieuses de leur alimentation. Elle est également une bonne source de vitamines et de minéraux, tels que la vitamine K, la vitamine C et le folate. La laitue peut être dégustée seule ou accompagnée d'autres légumes, de protéines ou de vinaigrettes pour créer des salades savoureuses et rafraîchissantes."
 )
-
 file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686312753/salade_bnvqck.jpg')
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Petit pois",
   variety: "Fabaceae",
   min_temp: 5,
@@ -187,7 +182,7 @@ file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686312753/p
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Haricot vert",
   variety: "Fabacées",
   min_temp: 16,
@@ -198,12 +193,11 @@ Vegetable.create!(
   footprint: 0.1,
   description: "Les haricots verts sont de minces et longues gousses vertes qui sont un type de légume communément consommé. Ils ont une texture croquante et une saveur douce et délicate. Les haricots verts sont une excellente source de nutriments, tels que les vitamines A, C et K, ainsi que de fibres alimentaires. Ils sont également faibles en calories, ce qui en fait un choix sain pour les régimes alimentaires équilibrés. Les haricots verts peuvent être cuits à la vapeur, sautés, blanchis ou ajoutés à des plats tels que les salades, les ragoûts, les sautés ou les plats d'accompagnement. Ils sont souvent appréciés pour leur couleur vibrante, leur goût frais et leur capacité à ajouter de la texture et de la nutrition à une variété de repas."
 )
-
 file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686312752/haricotvert_ufwzwk.jpg')
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Poivron",
   variety: "Capsicum annuum",
   min_temp: 18,
@@ -214,12 +208,11 @@ Vegetable.create!(
   footprint: 0.3,
   description: "Le poivron est un légume coloré et croquant largement apprécié pour sa saveur douce et sa polyvalence culinaire. Il existe différentes variétés de poivrons, notamment les poivrons rouges, verts et jaunes. Ils sont riches en vitamines C, A et en antioxydants bénéfiques pour la santé. Les poivrons sont utilisés dans de nombreux plats, tels que les sautés, les salades, les sauces et les plats grillés. Leur texture croquante et leur goût sucré les rendent délicieux aussi bien crus que cuits. Les poivrons ajoutent de la couleur, de la saveur et des nutriments aux repas et peuvent être appréciés par les amateurs de légumes et les gourmets."
 )
-
 file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686312753/poivron_wuq5mh.jpg')
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Courgette",
   variety: "Cucurbitaceae",
   min_temp: 21,
@@ -234,7 +227,7 @@ file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686312733/c
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Maïs",
   variety: "Cucurbitaceae",
   min_temp: 12,
@@ -245,12 +238,11 @@ Vegetable.create!(
   footprint: 0.12,
   description: "Le maïs est un légume populaire et polyvalent, largement consommé dans le monde entier. Il est caractérisé par ses grains jaunes ou blancs disposés en rangées sur une épaisse tige. Le maïs est apprécié pour sa saveur douce et sucrée. Il est une excellente source de fibres alimentaires, de vitamines et de minéraux tels que la vitamine C, le folate et le magnésium. Le maïs peut être consommé de différentes manières, que ce soit frais, en conserve, en épi, en grains ou transformé en farine pour la préparation de produits de boulangerie. Il est utilisé dans de nombreux plats, tels que les salades, les soupes, les tacos, les enchiladas et les plats d'accompagnement. Le maïs est apprécié pour sa polyvalence culinaire et sa capacité à ajouter de la douceur et de la texture à de nombreux plats."
 )
-
 file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686312752/mais_szodom.jpg')
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Tomate",
   variety: "Solanaceae",
   min_temp: 12,
@@ -261,12 +253,11 @@ Vegetable.create!(
   footprint: 0.15,
   description: "La tomate est un fruit-légume rouge vif et juteux, souvent considéré comme un légume dans la cuisine. Elle est caractérisée par sa texture lisse et sa saveur légèrement acidulée et sucrée. La tomate est une excellente source de vitamines C, A et K, ainsi que de minéraux tels que le potassium. Elle contient également des antioxydants bénéfiques pour la santé. Les tomates sont utilisées dans de nombreux plats, que ce soit crues dans les salades, les sandwichs et les salsas, ou cuites dans les sauces, les soupes, les plats mijotés et les pizzas. Elles apportent une saveur rafraîchissante et une couleur vive aux plats. La tomate est un aliment polyvalent et nutritif qui est apprécié pour sa fraîcheur et son goût délicieux."
 )
-
 file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686312753/tomate_zhk0vq.jpg')
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Persil",
   variety: "Apiaceae",
   min_temp: 7,
@@ -277,12 +268,11 @@ Vegetable.create!(
   footprint: 0.05,
   description: "Le persil est une herbe aromatique couramment utilisée en cuisine pour sa saveur fraîche et son parfum caractéristique. Il est souvent utilisé comme garniture ou ingrédient dans de nombreux plats et recettes. Le persil est riche en vitamines, notamment la vitamine C, et en minéraux tels que le fer et le potassium. Il est également connu pour ses propriétés antioxydantes et ses bienfaits pour la digestion. Le persil peut être utilisé frais ou séché, ajouté aux salades, aux soupes, aux sauces, aux plats de viande, de poisson ou de légumes pour apporter une saveur herbacée et une touche de fraîcheur. C'est une herbe polyvalente et largement appréciée dans de nombreuses cuisines à travers le monde."
 )
-
 file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686312753/persil_qtmwuc.jpg')
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Basilic",
   variety: "Lamiaceae",
   min_temp: 11,
@@ -293,12 +283,11 @@ Vegetable.create!(
   footprint: 0.03,
   description: "Le basilic est une herbe aromatique populaire, largement utilisée dans la cuisine méditerranéenne et asiatique. Il est apprécié pour son arôme distinctif et son goût légèrement sucré avec des notes de clou de girofle et de poivre. Le basilic est une source de vitamines A, K et C, ainsi que de minéraux tels que le potassium et le calcium. Il est souvent utilisé frais dans les salades, les sauces, les pestos et les plats de pâtes. Le basilic est également un ingrédient clé dans la préparation du célèbre plat italien, la caprese. Son parfum et sa saveur ajoutent une touche de fraîcheur et d'intensité à de nombreux plats. Le basilic est une herbe polyvalente et appréciée pour ses qualités gustatives et aromatiques uniques."
 )
-
 file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686312323/basilic_a3bbol.jpg')
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Oignon",
   variety: "Liliaceae",
   min_temp: 8,
@@ -313,7 +302,7 @@ file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686312752/o
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Poireau",
   variety: "Liliaceae",
   min_temp: 3,
@@ -328,7 +317,7 @@ file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686312753/p
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Citron",
   variety: "",
   min_temp: 18,
@@ -339,12 +328,11 @@ Vegetable.create!(
   footprint: 1.8,
   description: "Le citron est un agrume jaune vif et acide, caractérisé par son goût frais et acidulé. Il est largement utilisé en cuisine et en boisson pour sa saveur distinctive. Le citron est une excellente source de vitamine C, d'antioxydants et de minéraux tels que le potassium. Son jus est souvent utilisé comme assaisonnement ou pour rehausser la saveur des plats, des salades, des boissons et des desserts. Le zeste de citron est également utilisé pour ajouter une touche d'arôme et de parfum. Le citron est apprécié pour sa capacité à apporter de la fraîcheur, de l'acidité et une note citrique aux préparations culinaires."
 )
-
 file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686565635/citron_waicvp.jpg')
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Avocat",
   variety: "",
   min_temp: 23,
@@ -359,7 +347,7 @@ file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686565664/a
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Banane",
   variety: "",
   min_temp: 25,
@@ -370,12 +358,11 @@ Vegetable.create!(
   footprint: 2.5,
   description: "La banane est un fruit en forme de courbe, généralement jaune à maturité, et doté d'une chair douce et crémeuse. Elle est connue pour sa saveur sucrée et sa texture agréable. La banane est une excellente source de nutriments, tels que les vitamines C et B6, le potassium, les fibres et les antioxydants. Elle est souvent consommée crue et peut être utilisée dans de nombreuses préparations culinaires, notamment les smoothies, les salades de fruits, les desserts et les pains. La banane est appréciée pour son goût naturellement sucré, sa facilité à être transportée et sa capacité à fournir une énergie rapide. C'est un fruit polyvalent et populaire dans le monde entier."
 )
-
 file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686565710/banane_xeb1ao.jpg')
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
 
-Vegetable.create!(
+vegetable = Vegetable.new(
   name: "Mangue",
   variety: "",
   min_temp: 29,
@@ -386,7 +373,6 @@ Vegetable.create!(
   footprint: 2.1,
   description: "La mangue est un fruit tropical à la peau lisse et à la chair juteuse et sucrée. Elle est appréciée pour sa saveur exotique et son parfum sucré et floral. La mangue est riche en vitamines C et A, en fibres et en antioxydants. Elle est souvent consommée crue et peut également être utilisée dans une variété de plats sucrés et salés, tels que les salades, les smoothies, les sauces, les glaces et les desserts. La texture de la mangue est douce et fondante, ce qui en fait un fruit délicieux et rafraîchissant à déguster. La mangue est très appréciée dans les régions tropicales et elle est également exportée dans le monde entier."
 )
-
 file = URI.open('https://res.cloudinary.com/dasx2arbl/image/upload/v1686565644/mangue_xhrbtq.jpg')
 vegetable.photo.attach(io: file, filename: "veg.jpg", content_type: "image/png")
 vegetable.save
