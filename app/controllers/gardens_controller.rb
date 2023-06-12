61ea4671f1cb65aa20efd91815a3ec126580d73c
brice blanchard <brice.blanchard21@yahoo.fr>
2023-06-12 17:00:13 +0200

    added validation error message at garden creation

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -24,18 +24,25 @@ class GardensController < ApplicationController
   def create
     @garden = Garden.new(garden_params)
     @garden.user = current_user
-    # Calculate number of compartments
-    n = number_of_compartments(@garden.width)
-    m = number_of_implantations(@garden.length)
-    i = 0
-    while (i <= n) do
-      @garden.compartments.new
-      i += 1
-    end
-    if @garden.save!
-      redirect_to garden_created_path(@garden)
+    if @garden.valid?
+      # Calculate number of compartments
+      n = number_of_compartments(@garden.width)
+      m = number_of_implantations(@garden.length)
+      i = 0
+      while (i <= n) do
+        @garden.compartments.new
+        i += 1
+      end
+      if @garden.save!
+        redirect_to garden_created_path(@garden)
+        # flash.notice = "Garden succesfully created!"
+      else
+        flash.alert = "Error in creating garden..."
+        render :new, status: :unprocessable_entity
+      end
     else
-      render new, status: :unprocessable_entity
+      flash.alert = "Error in creating garden..."
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -47,7 +54,6 @@ class GardensController < ApplicationController
   end
 
   def implant
-    raise
     @choices = params.select { |key, value| key.to_s.match("vegetable") }
     @array_of_veggie = @choices.values
     @implantation = get_synergies(@array_of_veggie, @vegetables_for_weather, @garden.length)
