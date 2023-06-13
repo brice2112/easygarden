dcdd0fbb5a45dd0085cc14c19d1cd77e3d472926
Helene Coulombeix <helenecoulombeix@yahoo.fr>
2023-06-13 12:38:22 +0200

    mardi2matin

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -8,8 +8,7 @@ class Garden < ApplicationRecord
   # geocoded_by :location
 
   validates :name, presence: true
-  validates :length, presence: true, numericality: { only_integer: true, greater_than: 0,  less_than_or_equal_to: 50,  message: "must be between 0 and 50" }
-  validates :width, presence: true, numericality: { only_integer: true, greater_than: 0,  less_than_or_equal_to: 50,  message: "must be between 0 and 50" }
+  validates :length, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 50 }
+  validates :width, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 50 }
   validates :location, presence: true
-
 end
