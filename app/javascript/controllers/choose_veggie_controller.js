f028eaa18bc3b41ea6b756f831a0d6e33a432bfc
Olivier Pothron <olivier.pothron@gmail.com>
2023-06-14 09:31:41 +0200

    Stimulus Changed

--- app/javascript/controllers/choose_veggie_controller.js
+++ app/javascript/controllers/choose_veggie_controller.js
@@ -2,74 +2,43 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="choose-veggie"
 export default class extends Controller {
-  static targets = ["field"]
+  static targets = ["comp", "field"]
 
   connect() {
     this.counter = 0
+    this.compCount = this.compTargets.length;
     this.vegetable_ids = []
     console.log(this.fieldTarget);
   }
 
   addVegetable(event) {
-    console.log("add");
-    const id = event.currentTarget.dataset.id
-    console.log(id);
-    this.vegetable_ids.push(id)
-    console.log(this.vegetable_ids);
-    this.updateField(id)
+    if (this.counter >= this.compCount) return;
 
+    console.log("add");
+    const id = event.currentTarget.dataset.id;  //gets the #id of the vegetable in the card
+    this.vegetable_ids.push(id);                //adds the #id to the vegetable_ids array
+    this.updateField(id);
+    this.counter += 1;
   }
 
   removeVegetable(event) {
     const id = event.currentTarget.dataset.id
     if (!this.vegetable_ids.includes(id)) return;
 
-    console.log("je remove");
-
-    const index = this.vegetable_ids.indexOf(id);
+    console.log("remove");
+    const index = this.vegetable_ids.indexOf(id); //gets the position of the #id to remove
     if (index > -1) { // only splice array when item is found
       this.vegetable_ids.splice(index, 1); // 2nd parameter means remove one item only
     }
-    console.log(this.vegetable_ids);
-
     this.updateField(id)
+    this.counter -= 1;
   }
 
   updateField(id) {
-    const counterDiv = document.querySelector(`#counter_${id}`)
-    const counter = this.vegetable_ids.filter(x => x == id).length
-    counterDiv.innerText = counter
-    this.fieldTarget.value = this.vegetable_ids
+    const counterDiv = document.querySelector(`#counter_${id}`)  //select the <span #id> of the vegetable in the card
+    const counter = this.vegetable_ids.filter(x => x == id).length  //counts the number of occurence of #id of given vegetable
+    counterDiv.innerText = counter                                //prints the number in the <span> element
+    this.fieldTarget.value = this.vegetable_ids                   //updates the value of the 'toto' field
     console.log(this.fieldTarget.value);
   }
-
-  // compCount = this.compTargets.length;
-  // counter = 0;
-
-  // chosen(event) {
-  //   var get_veggie = (event.target.dataset.value);
-  //   this.addIntoCompartment(get_veggie);
-  // }
-
-  // addIntoCompartment(element) {
-  //   if (this.counter < this.compCount) {
-  //     console.log(this.counter);
-  //     this.compTargets[this.counter].textContent = element;
-  //     this.counter++;
-  //     this.checkButtonState();
-  //   } else {
-  //     console.log("CannotAddAdditionnalVegetable");
-  //   }
-  // }
-
-  // checkButtonState() {
-  //   if (this.counter === this.compCount) {
-  //     this.buttonTarget.disabled = false;
-  //     this.vegeTargets.forEach(element => {
-  //       console.log(element);
-  //       element.classList.toggle('active');
-  //     });
-
-  //   }
-  // }
 }
