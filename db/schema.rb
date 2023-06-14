5bdb0b61987e12b9e2acb87eb26fa543074fd536
Helene Coulombeix <helenecoulombeix@yahoo.fr>
2023-06-14 15:32:22 +0200

    geocoder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_14_084637) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_14_132240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_14_084637) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "mean_temperature"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_gardens_on_user_id"
   end
 
